edd7a450bce80f5dce9c6e6e93b926869f7d2f6c
yuga <shohei.murayama@gmail.com>
2012-11-08 14:07:12 +0900

    added an implementation of BootStrappedHeap in Section 10.2.2

---
+++ OCaml/bootStrappedHeap.ml
@@ -0,0 +1,102 @@
+open Heap;;
+open Ordered;;
+
+module BootStrappedHeap
+ = functor (MakeH : functor (Element : ORDERED) -> HEAP with module Elem = Element)
+-> functor (Element : ORDERED)
+-> struct
+  module Elem = Element
+
+  module rec BootStrappedElem : sig
+    type t = E
+           | H of Elem.t * PrimH.heap
+    exception Empty
+    include ORDERED with type t := t
+  end = struct
+    type t = E
+           | H of Elem.t * PrimH.heap
+    exception Empty
+    
+    let eq = function
+      | E, E -> true
+      | (H (x, _), (H (y, _))) -> Elem.eq (x, y)
+      | _, _ -> false
+    ;;
+
+    let lt = function
+      | (H (x, _), (H (y, _))) -> Elem.lt (x, y)
+      | _ -> raise Empty
+    ;;
+
+    let leq = function
+      | (H (x, _), (H (y, _))) -> Elem.leq (x, y)
+      | _ -> raise Empty
+    ;;
+
+    let print = function
+      | E -> print_string "E"
+      | H (x, h) ->
+          print_string "H (";
+          Elem.print x;
+          print_string ", h)"
+    ;;
+  end
+  and PrimH : HEAP with module Elem := BootStrappedElem = MakeH (BootStrappedElem);;
+
+  include BootStrappedElem
+
+  type heap = t
+
+  let empty = E
+  ;;
+
+  let isEmpty = function
+    | E -> true
+    | _ -> false
+  ;;
+
+  let merge = function
+    | (E, h) -> h
+    | (h, E) -> h
+    | ((H (x, p1) as h1), (H (y, p2) as h2)) ->
+        if Elem.leq (x, y) then
+          H (x, PrimH.insert (h2, p1))
+        else
+          H (y, PrimH.insert (h1, p2))
+  ;;
+
+  let insert (x, h) = merge (H (x, PrimH.empty), h)
+  ;;
+
+  let findMin = function 
+    | E -> raise Empty
+    | H (x, _) -> x
+  ;;
+
+  let deleteMin = function
+    | E -> raise Empty
+    | H (x, p) ->
+        if PrimH.isEmpty p then
+          E
+        else
+          let H (y, p1) = PrimH.findMin p in
+          let p2 = PrimH.deleteMin p in
+          H (y, PrimH.merge (p1, p2))
+  ;;
+
+  let print = function
+    | E ->
+        print_string "E";
+        print_newline()
+    | H (x, p) ->
+        print_string "H (";
+        Elem.print x;
+        print_string ",\n";
+        PrimH.print p;
+        print_string ")";
+        print_newline()
+  ;;
+end
+
+open ScheduledBinomialHeap;;
+module IntBootStrappedHeap : HEAP with module Elem = Int = BootStrappedHeap (ScheduledBinomialHeap) (Int)
---
+++ OCaml/runTestBootStrappedHeap.ml
@@ -0,0 +1,30 @@
+#use "topfind";;
+#load "item.cmo";;
+#load "ordered.cmo";;
+#load "smallStream.cmo";;
+#load "heap.cmo";;
+#load "scheduledBinomialHeap.cmo";;
+#load "bootStrappedHeap.cmo";;
+
+open BootStrappedHeap;;
+module H = IntBootStrappedHeap
+
+let make_heap n =
+  let rec make_heap n i h =
+    if i < n
+    then make_heap n (i+1) (H.insert (i, h))
+    else H.insert (i, h)
+  in make_heap n 1 H.empty
+;;
+
+print_string "n=0\n"; H.print H.empty;;
+print_string "n=1\n"; H.print (make_heap 1);;
+print_string "n=2\n"; H.print (make_heap 2);;
+print_string "n=3\n"; H.print (make_heap 3);;
+print_string "n=4\n"; H.print (make_heap 4);;
+print_string "n=5\n"; H.print (make_heap 5);;
+print_string "n=6\n"; H.print (make_heap 6);;
+print_string "n=7\n"; H.print (make_heap 7);;
+print_string "n=8\n"; H.print (make_heap 8);;
+print_string "n=9\n"; H.print (make_heap 9);;
+print_string "n=1023\n"; H.print (make_heap 1023);;
